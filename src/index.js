9bcc9ce4d2773b1f258452fcb18aa2e929543abe
Santiago <santiagofonsecaf311@gmail.com>
2023-10-08 12:10:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { App } from 'components/App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import { apiSlice } from 'redux/contact';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
